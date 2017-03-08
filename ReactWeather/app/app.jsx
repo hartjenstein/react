0b7860660ef3f5b4821acce196de2c53a2c3366f
Nick Hartjenstein <nick@iMac.local>
2017-03-08 09:04:38 +0100

    *

---
+++ ReactWeather/app/app.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+let { Route, Router, IndexRoute, HashHistory } = require('react-router'); 
+
+ReactDOM.render(
+  <h1>Boilerplate app!</h1>,
+  document.getElementById('app')
+);
