f0e516c9f33ee0732d7af20e400710cfbee0ef2d
Nick Hartjenstein <nick@iMac.local>
2017-03-15 11:12:24 +0100

    *

--- server.js
+++ server.js
@@ -3,19 +3,17 @@ var express = require('express');
 // Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.static('public'));
 
-
-app.use((req, res, next) =>  {
-  if(req.headers['x-forwarded-proto'] === 'http') {
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
     res.redirect('http://' + req.hostname + req.url);
   }
-}); 
-//test
+});
+
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log('Express server is up on port 3000' + PORT);
+  console.log('Express server is up on port ' + PORT);
 });
