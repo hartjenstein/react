ddf4316fd4e9de16f32d99bde2d6bca8f1f3733d
Nick Hartjenstein <nick@iMac.local>
2017-03-15 10:04:49 +0100

    *

--- helloReact/server.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-
-let app = express();
-
-app.use(express.static('public'));
-
-app.listen(3000, () => {
-    console.log('Express server is up on port 3000');
-} )
