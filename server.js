e70386b84c7a5bf4d52e3fafc53086725fb0d7bf
Nick Hartjenstein <nick@iMac.local>
2017-03-15 11:49:31 +0100

    modified title of about and examples module

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ const PORT = process.env.PORT || 3000;
 // redirecting https traffic from heroku to http because the openweathermaps free plan only supports http
 // side note - arrow function does not work for the function below -results in type error.
 app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http') {
+  if (req.headers['x-forwarded-proto'] === 'https') {
    res.redirect('http://' + req.hostname + req.url);
   } else {
-    
      next();
   }
 });
