a0107ca6ca68083e4256eab5a8dc710e841e0a32
Nick Hartjenstein <nick@iMac.local>
2017-03-15 11:28:23 +0100

    *

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+// redirecting https traffic from heroku to http because the openweathermaps free plan only supports http
+// side note - arrow function does not work for the function below -results in type error.
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
+   res.redirect('http://' + req.hostname + req.url);
   } else {
-    res.redirect('http://' + req.hostname + req.url);
+    
+     next();
   }
 });
 
