f0f9a2285c1724e33c20e88c11d91375f6b345f2
Nick Hartjenstein <nick@iMac.local>
2017-03-16 11:27:06 +0100

    added modal

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -3,34 +3,41 @@ let WeatherForm = require('WeatherForm');
 let WeatherMessage = require('WeatherMessage');
 let Nav = require('Nav');
 let openWeatherMap = require('openWeatherMap');
+let ErrorModal = require('ErrorModal');
 
 let Weather = React.createClass({
 
   // getInitiaState is built in to react so its getting called automatically 
   getInitialState: function () {
     return {
-        isLoading: false
+        isLoading: false,
     };
   },
   handleNewData: function(location) {
-  //  let that = this;
-    this.setState({isLoading: true});
-    openWeatherMap.getTemp(location).then( (temp) => {
-        this.setState({
+  
+    let that = this;
+    this.setState({
+      isLoading: true,
+      errorMessage: undefined
+    });
+
+  openWeatherMap.getTemp(location).then(function (temp) {
+    that.setState({
         location: location,
         temp: temp,
         isLoading: false
       });
-    }, (errorMessage) => {
-      this.setState({
-        isLoading: false
+  }, function (e) {
+    that.setState({
+        isLoading: false,
+        errorMessage: e.message
       });
-      alert(errorMessage);
     });
   },
   
   render: function () {
-    let { isLoading, location, temp } = this.state;
+    let { isLoading, location, temp, errorMessage } = this.state;
+
     let renderMessage = () => {
       if(isLoading) { 
         return <h3 className="text-centered">Fetching weather...</h3>;
@@ -38,11 +45,20 @@ let Weather = React.createClass({
         return <WeatherMessage temp={temp} location={location}/>;
       }
     };
+    function renderError(){
+      if(typeof errorMessage === 'string') {
+        return (
+          <ErrorModal message={errorMessage}/>
+        );
+      }
+    }
+  
     return (
       <div>
         <h1 className="text-center">Get Weather</h1>
         <WeatherForm onNewData={this.handleNewData}/>
         {renderMessage()}
+        {renderError()}
       </div>
     );
   }
